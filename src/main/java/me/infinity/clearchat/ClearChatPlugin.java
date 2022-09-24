b13d4179b18e0962ff84819a7ecddba62b4b20cf
Infinity <xa583179@gmail.com>
2022-09-24 10:37:36 +0530

    commands

--- src/main/java/me/infinity/clearchat/ClearChatPlugin.java
+++ src/main/java/me/infinity/clearchat/ClearChatPlugin.java
@@ -1,6 +1,8 @@
 package me.infinity.clearchat;
 
+import co.aikar.commands.BukkitCommandManager;
 import lombok.Getter;
+import me.infinity.clearchat.command.ClearChatCommand;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -16,6 +18,10 @@ public final class ClearChatPlugin extends JavaPlugin {
         this.settings = this.getConfig().getConfigurationSection("SETTINGS");
         this.messages = this.getConfig().getConfigurationSection("MESSAGES");
         this.manager = new ClearChatManager(this);
+
+        BukkitCommandManager commandManager = new BukkitCommandManager(this);
+        commandManager.registerCommand(new ClearChatCommand(this));
+
     }
 
     @Override
